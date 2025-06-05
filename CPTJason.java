67ebe32497a7262106ae6cc8ec0259cf4f6cba0e
Walker21122008 <hasini.vijayinbasri26@ycdsbk12.ca>
2025-06-05 11:41:00 -0400

    added changes

--- CPTJason.java
+++ CPTJason.java
@@ -21,7 +21,7 @@ public class CPTJason {
         BufferedImage imgQuizchoiceBKG = con.loadImage("Quizchoice.png");
         BufferedImage imgJokesBKG = con.loadImage("Jokes.png");
 
-        // Game variables
+
         String strName = "";
         int intScore = 0;
         String strAnswer = "";
@@ -29,171 +29,232 @@ public class CPTJason {
         int intQuizchoice = 0;
         String quizName = "";
 
-        // Main menu
-        con.drawImage(imgHomePageBKG, 0, 0);
-        con.println("Calc and Vectors Multiple Choice Game");
-        con.println("Press P to play game");
-        con.println("Press V to view High Scores");
-        con.println("Press H for help options");
-        con.println("Press Q to exit");
-        con.println("Try pressing J"); 
-        String strInput = con.readLine();
+        boolean running = true;
 
-        if (strInput.equalsIgnoreCase("p")) {
-            con.drawImage(imgUsernameBKG, 0, 0);
-            con.println("Enter your username: ");
-            strName = con.readLine();
-            while (strName.equals("")) {
-                con.println("Username cannot be blank. Try again:");
-                strName = con.readLine();
-            }
-
-            if (strName.equalsIgnoreCase(strCheat)) {
-                intScore = 2;
-                con.println("Cheat enabled! Starting score: " + intScore);
-            }
-
-            String[] strQuizchoices = new String[3];
-            TextInputFile file = new TextInputFile("Quizchoice.txt");
-            for (int i = 0; i < 3; i++) {
-                strQuizchoices[i] = file.readLine();
-            }
-
-            con.drawImage(imgQuizchoiceBKG, 0, 0);
-            con.println("Please choose a quiz to complete:");
-            for (int i = 0; i < 3; i++) {
-                con.println((i + 1) + ") " + strQuizchoices[i]);
-            }
-            intQuizchoice = con.readInt();
-            quizName = strQuizchoices[intQuizchoice - 1];
-
-            String selectedQuizFile = "";
-            if (intQuizchoice == 1) {
-                selectedQuizFile = "Calc.txt";
-                con.drawImage(imgChoice1BKG, 0, 0);
-            } else if (intQuizchoice == 2) {
-                selectedQuizFile = "Vectors.txt";
-                con.drawImage(imgChoice2BKG, 0, 0);
-            } else if (intQuizchoice == 3) {
-                selectedQuizFile = "Both.txt";
-                con.drawImage(imgChoice3BKG, 0, 0);
-            }
-
+        while (running) {
+			con.setTextColor(Color.BLACK);
+            con.setDrawColor(Color.BLACK);
             con.clear();
-            TextInputFile quiz = new TextInputFile(selectedQuizFile);
-            int intNumQuestions = 0;
-            while (!quiz.eof()) {
-                quiz.readLine();
-                intNumQuestions++;
-            }
-            quiz.close();
-            intNumQuestions /= 6;
+            con.drawImage(imgHomePageBKG, 0, 0);
+            con.println("Try pressing J");
+            String strInput = con.readLine();
 
-            String[][] strQuiz = new String[intNumQuestions][6];
-            TextInputFile quiz2 = new TextInputFile(selectedQuizFile);
-            for (int i = 0; i < intNumQuestions; i++) {
-                for (int j = 0; j < 6; j++) {
-                    strQuiz[i][j] = quiz2.readLine();
-                }
-            }
-
-            // Shuffle questions
-            Random random = new Random();
-            for (int i = 0; i < intNumQuestions; i++) {
-                int randIndex = random.nextInt(intNumQuestions);
-                String[] temp = strQuiz[i];
-                strQuiz[i] = strQuiz[randIndex];
-                strQuiz[randIndex] = temp;
-            }
-
-            for (int i = 0; i < intNumQuestions; i++) {
+            if (strInput.equalsIgnoreCase("p")) {
+                con.setDrawColor(Color.BLACK);
                 con.clear();
-                con.println("Q" + (i + 1) + ": " + strQuiz[i][0]);
-                con.println("A) " + strQuiz[i][1]);
-                con.println("B) " + strQuiz[i][2]);
-                con.println("C) " + strQuiz[i][3]);
-                con.println("D) " + strQuiz[i][4]);
-                con.print("Enter your answer (A/B/C/D): ");
-                strAnswer = con.readLine().toUpperCase();
-
-                if (strAnswer.equals(strQuiz[i][5].toUpperCase())) {
-                    intScore++;
+                con.drawImage(imgUsernameBKG, 0, 0);
+                con.println("\n\n\n\n\n\n\n\n\n\n");
+                con.print("                                          ");
+                strName = con.readLine();
+                while (strName.equals("")) {
+                    con.println("Username cannot be blank. Try again:");
+                    strName = con.readLine();
                 }
-            }
 
-            con.drawImage(imgScoreBKG, 0, 0);
-            con.println(strName + ", your final score is: " + intScore + "/" + intNumQuestions);
+                intScore = 0;
+                if (strName.equalsIgnoreCase(strCheat)) {
+                    intScore = 2;
+                    con.println("Cheat enabled! Starting score: " + intScore);
+                }
 
-            // Save score
-            TextOutputFile writer = new TextOutputFile("/Users/jasonguan/Documents/GitHub/JasonGuan-afk-Cpt/HighScore.txt", true);
-            writer.println(strName + "," + quizName + "," + intScore);
-            writer.close();
+                String[] strQuizchoices = new String[3];
+                TextInputFile file = new TextInputFile("Quizchoice.txt");
+                for (int i = 0; i < 3; i++) {
+                    strQuizchoices[i] = file.readLine();
+                }
+                file.close();
 
-        } else if (strInput.equalsIgnoreCase("v")) {
-            TextInputFile scoreReader = new TextInputFile("/Users/jasonguan/Documents/GitHub/JasonGuan-afk-Cpt/HighScore.txt");
+                con.setDrawColor(Color.BLACK);
+                con.clear();
+                con.drawImage(imgQuizchoiceBKG, 0, 0);
+                con.println("Please choose a quiz to complete:");
+                for (int i = 0; i < 3; i++) {
+                    con.println((i + 1) + ") " + strQuizchoices[i]);
+                }
+                intQuizchoice = con.readInt();
+                while (intQuizchoice < 1 || intQuizchoice > 3) {
+                    con.println("Invalid choice. Please enter 1, 2, or 3:");
+                    intQuizchoice = con.readInt();
+                }
+                quizName = strQuizchoices[intQuizchoice - 1];
 
-            // Read scores
-            String[] names = new String[100];
-            String[] quizzes = new String[100];
-            int[] scores = new int[100];
-            int count = 0;
+                String selectedQuizFile = "";
+                if (intQuizchoice == 1) {
+                    selectedQuizFile = "Calc.txt";
+                    con.drawImage(imgChoice1BKG, 0, 0);
+                } else if (intQuizchoice == 2) {
+                    selectedQuizFile = "Vectors.txt";
+                    con.drawImage(imgChoice2BKG, 0, 0);
+                } else if (intQuizchoice == 3) {
+                    selectedQuizFile = "Both.txt";
+                    con.drawImage(imgChoice3BKG, 0, 0);
+                }
 
-            while (!scoreReader.eof()) {
-                String line = scoreReader.readLine();
-                if (line != null && !line.equals("")) {
-                    String[] parts = line.split(",");
-                    if (parts.length == 3) {
-                        names[count] = parts[0];
-                        quizzes[count] = parts[1];
-                        scores[count] = Integer.parseInt(parts[2]);
-                        count++;
+                con.clear();
+                TextInputFile quiz = new TextInputFile(selectedQuizFile);
+                int intNumQuestions = 0;
+                while (!quiz.eof()) {
+                    quiz.readLine();
+                    intNumQuestions++;
+                }
+                quiz.close();
+                intNumQuestions /= 6;
+
+                String[][] strQuiz = new String[intNumQuestions][6];
+                TextInputFile quiz2 = new TextInputFile(selectedQuizFile);
+                for (int i = 0; i < intNumQuestions; i++) {
+                    for (int j = 0; j < 6; j++) {
+                        strQuiz[i][j] = quiz2.readLine();
                     }
                 }
-            }
+                quiz2.close();
 
-            // Bubble sort by score descending
-            for (int i = 0; i < count - 1; i++) {
-                for (int j = 0; j < count - i - 1; j++) {
-                    if (scores[j] < scores[j + 1]) {
-                        // Swap scores
-                        int tempScore = scores[j];
-                        scores[j] = scores[j + 1];
-                        scores[j + 1] = tempScore;
+                // Shuffle questions
+                Random random = new Random();
+                for (int i = 0; i < intNumQuestions; i++) {
+                    int randIndex = random.nextInt(intNumQuestions);
+                    String[] temp = strQuiz[i];
+                    strQuiz[i] = strQuiz[randIndex];
+                    strQuiz[randIndex] = temp;
+                }
 
-                        // Swap names
-                        String tempName = names[j];
-                        names[j] = names[j + 1];
-                        names[j + 1] = tempName;
+                for (int i = 0; i < intNumQuestions; i++) {
+                    con.setDrawColor(Color.BLACK);
+                    con.clear();
+                    con.println("Q" + (i + 1) + ": " + strQuiz[i][0]);
+                    con.println("A) " + strQuiz[i][1]);
+                    con.println("B) " + strQuiz[i][2]);
+                    con.println("C) " + strQuiz[i][3]);
+                    con.println("D) " + strQuiz[i][4]);
+                    con.print("Enter your answer (A/B/C/D): ");
+                    strAnswer = con.readLine().toUpperCase();
 
-                        // Swap quizzes
-                        String tempQuiz = quizzes[j];
-                        quizzes[j] = quizzes[j + 1];
-                        quizzes[j + 1] = tempQuiz;
+                    while (!strAnswer.equals("A") && !strAnswer.equals("B") && !strAnswer.equals("C") && !strAnswer.equals("D")) {
+                        con.print("Invalid answer. Please enter A, B, C, or D: ");
+                        strAnswer = con.readLine().toUpperCase();
+                    }
+
+                    if (strAnswer.equals(strQuiz[i][5].toUpperCase())) {
+                        intScore++;
                     }
                 }
-            }
 
-            // Display
-            con.clear();
-            con.println("=== High Scores ===");
-            for (int i = 0; i < count && i < 10; i++) {
-                con.println((i + 1) + ". " + names[i] + " | " + quizzes[i] + " | " + scores[i]);
-            }
+                boolean postGame = true;
+                while (postGame) {
+                    con.setDrawColor(Color.BLACK);
+                    con.clear();
+                    con.drawImage(imgScoreBKG, 0, 0);
+                    con.println(strName + ", your final score is: " + intScore + "/" + intNumQuestions);
+                    con.println();
+                    con.println("Press M to return to the main menu and save your score.");
+                    con.println("Press E to exit without saving.");
 
-        } else if (strInput.equalsIgnoreCase("h")) {
-            con.drawImage(imgHelpBKG, 0, 0);
-            con.println("Help Section:");
-            con.println("- Enter your username to begin.");
-            con.println("- Choose a quiz topic.");
-            con.println("- Answer each question (A/B/C/D).");
-            con.println("- Use 'statitan' as a cheat code for bonus points!");
-        } else if (strInput.equalsIgnoreCase("q")) {
-            con.println("Thanks for playing!");
-        } else if (strInput.equalsIgnoreCase("j")) {
-            con.clear();
-            con.drawImage(imgJokesBKG, 0, 0);
-        } else {
-            con.println("Invalid choice. Restart the program.");
+                    String postGameInput = con.readLine();
+
+                    if (postGameInput.equalsIgnoreCase("M")) {
+                        // Save score
+                        TextOutputFile writer = new TextOutputFile("HighScore.txt", true);
+                        writer.println(strName + "," + quizName + "," + intScore);
+                        writer.close();
+                        con.println("Score saved! Returning to main menu...");
+                        con.sleep(1500);
+                        postGame = false; // return to main menu
+                    } else if (postGameInput.equalsIgnoreCase("E")) {
+                        con.println("Thanks for playing!");
+                        con.sleep(1500);
+                        running = false; // exit program
+                        postGame = false;
+                    } else {
+                        con.println("Invalid input. Please try again.");
+                        con.sleep(1000);
+                    }
+                }
+
+            } else if (strInput.equalsIgnoreCase("v")) {
+                TextInputFile scoreReader = new TextInputFile("HighScore.txt");
+
+                String[] names = new String[100];
+                String[] quizzes = new String[100];
+                int[] scores = new int[100];
+                int count = 0;
+
+                while (!scoreReader.eof()) {
+                    String line = scoreReader.readLine();
+                    if (line != null && !line.equals("")) {
+                        String[] parts = line.split(",");
+                        if (parts.length == 3) {
+                            names[count] = parts[0];
+                            quizzes[count] = parts[1];
+                            try {
+                                scores[count] = Integer.parseInt(parts[2]);
+                            } catch (NumberFormatException e) {
+                                scores[count] = 0;
+                            }
+                            count++;
+                        }
+                    }
+                }
+                scoreReader.close();
+
+                // Bubble sort by score descending
+                for (int i = 0; i < count - 1; i++) {
+                    for (int j = 0; j < count - i - 1; j++) {
+                        if (scores[j] < scores[j + 1]) {
+                            int tempScore = scores[j];
+                            scores[j] = scores[j + 1];
+                            scores[j + 1] = tempScore;
+
+                            String tempName = names[j];
+                            names[j] = names[j + 1];
+                            names[j + 1] = tempName;
+
+                            String tempQuiz = quizzes[j];
+                            quizzes[j] = quizzes[j + 1];
+                            quizzes[j + 1] = tempQuiz;
+                        }
+                    }
+                }
+
+                con.setDrawColor(Color.BLACK);
+                con.clear();
+                con.println("=== High Scores ===");
+                for (int i = 0; i < count && i < 10; i++) {
+                    con.println((i + 1) + ". " + names[i] + " | " + quizzes[i] + " | " + scores[i]);
+                }
+                con.println();
+                con.println("Press any key to return to main menu.");
+                con.readLine();
+
+            } else if (strInput.equalsIgnoreCase("h")) {
+                con.setDrawColor(Color.BLACK);
+                con.clear();
+                con.drawImage(imgHelpBKG, 0, 0);
+                con.println("Help Section:");
+                con.println("- Enter your username to begin.");
+                con.println("- Choose a quiz topic.");
+                con.println("- Answer each question (A/B/C/D).");
+                con.println("- Use 'statitan' as a cheat code for bonus points!");
+                con.println();
+                con.println("Press any key to return to main menu.");
+                con.readLine();
+
+            } else if (strInput.equalsIgnoreCase("e")) {
+                con.setDrawColor(Color.BLACK);
+                con.println("Thanks for playing!");
+                running = false;
+
+            } else if (strInput.equalsIgnoreCase("j")) {
+                con.setDrawColor(Color.BLACK);
+                con.clear();
+                con.drawImage(imgJokesBKG, 0, 0);
+                con.println("Press any key to return to main menu.");
+                con.readLine();
+
+            } else {
+                con.setDrawColor(Color.BLACK);
+                con.println("Invalid choice. Restart the program.");
+                con.sleep(1500);
+            }
         }
     }
 }
